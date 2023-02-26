678c1a8211174d3824ebe0e8ce63f7a9307aec7b
moloch54 <123097488+moloch54@users.noreply.github.com>
2023-02-27 00:05:00 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,5 +0,0 @@
-#/usr/bin/python
-
-import os
-
-os.system("sudo apt-get update; cd ~; sudo git clone https://github.com/moloch54/b4blood; sudo cp b4blood/b4blood.py /usr/bin/b4blood; sudo chmod +x /usr/bin/b4blood; sudo apt install -y python3-pip; sudo git clone https://github.com/fortra/impacket /opt/impacket; sudo apt install -y python3-impacket; cd /opt/impacket; sudo python3 ./setup.py install; cd /usr/share/wordlists; gunzip rockyou.txt.gz")
