a55aaa6d989ccb8507145d3835198b311d20f98c
moloch54 <123097488+moloch54@users.noreply.github.com>
2023-02-27 00:06:12 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+#/usr/bin/python
+
+import os
+
+os.system("sudo apt-get update")
+os.system(" sudo git clone https://github.com/moloch54/b4blood")
+os.system(" sudo cp b4blood/b4blood.py /usr/bin/b4blood")
+os.system(" sudo chmod +x /usr/bin/b4blood")
+os.system(" sudo apt install -y python3-pip")
+os.system(" sudo git clone https://github.com/fortra/impacket /opt/impacket")
+os.system(" sudo apt install -y python3-impacket")
+os.system(" cd /opt/impacket")
+os.system(" sudo python3 ./setup.py install")
+os.system(" gunzip /usr/share/wordlists/rockyou.txt.gz")
