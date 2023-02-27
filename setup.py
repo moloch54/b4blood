398ba60d0abf300c973ff3ef50f7fa67ccba71db
moloch54 <123097488+moloch54@users.noreply.github.com>
2023-02-27 03:08:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,4 +10,4 @@ os.system("sudo git clone https://github.com/fortra/impacket /opt/impacket")
 os.system("pip3 install -r /opt/impacket/requirements.txt")
 os.system("cd /opt/impacket/ && python3 ./setup.py install")
 if not os.path.isfile("/usr/share/wordlists/rockyou.txt"):
-	os.system("gunzip /usr/share/wordlists/rockyou.txt.gz")
+    os.system("gunzip /usr/share/wordlists/rockyou.txt.gz")
