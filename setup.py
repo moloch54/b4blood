1441906d88456cb810461bb2db52a6af0d261ee9
moloch54 <123097488+moloch54@users.noreply.github.com>
2023-02-27 02:01:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
-#/usr/bin/python
+#/usr/bin/python3
 
 import os
 
 os.system("sudo apt-get update")
 os.system("sudo install -y ntpdate")
-os.system("git clone https://github.com/TarlogicSecurity/kerbrute")
-os.system("pip install -r kerbrute/requirements.txt")
-os.system("sudo cp kerbrute/kerbrute.py /usr/bin/kerbrute")
-os.system("sudo git clone https://github.com/moloch54/b4blood")
-os.system("sudo cp b4blood/b4blood.py /usr/bin/b4blood")
+os.system("sudo cp b4blood.py /usr/bin/b4blood")
 os.system("sudo chmod +x /usr/bin/b4blood")
 os.system("sudo apt install -y python3-pip")
 os.system("sudo git clone https://github.com/fortra/impacket /opt/impacket")
 os.system("sudo apt install -y python3-impacket")
-os.system("cd /opt/impacket")
-os.system("sudo python3 ./setup.py install")
+os.system("sudo python3 /opt/impacket/setup.py install")
 os.system("gunzip /usr/share/wordlists/rockyou.txt.gz")
