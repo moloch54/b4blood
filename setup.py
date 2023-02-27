b8e17a1ae8ef6983d7d6842d07374816fab27cfc
moloch54 <123097488+moloch54@users.noreply.github.com>
2023-02-27 03:16:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.system("sudo install -y ntpdate")
 os.system("sudo cp b4blood.py /usr/bin/b4blood")
 os.system("sudo chmod +x /usr/bin/b4blood")
 os.system("sudo git clone https://github.com/fortra/impacket /opt/impacket")
-os.system("pip3 install -r /opt/impacket/requirements.txt")
+os.system("sudo apt install python3-impacket")
 os.system("cd /opt/impacket/ && python3 ./setup.py install")
 if not os.path.isfile("/usr/share/wordlists/rockyou.txt"):
     os.system("gunzip /usr/share/wordlists/rockyou.txt.gz")
